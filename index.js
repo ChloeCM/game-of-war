0a2ee62cc3b03dc6f2751a73f4ed6c95caed8630
Chloe Mills <chloecmills@gmail.com>
2025-08-06 13:22:56 +0100

    Added in functionalities like keeping score and updated the index.css

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const newDeckBtn = document.getElementById("new-deck");
 const drawCardBtn = document.getElementById("draw-card");
 const cardContainer = document.getElementById("cards");
+const headerEl = document.getElementById("header");
+const computerScoreEl = document.getElementById("computer-score");
+const myScoreEl = document.getElementById("my-score");
+const remainingCards = document.getElementById("remaining-cards");
 let deckId;
 
 function handleClick() {
@@ -30,5 +34,36 @@ drawCardBtn.addEventListener("click", function () {
       cardContainer.children[1].innerHTML = `
         <img src=${data.cards[1].image} class="card" />
       `;
+
+      const winnerCard = determineCardWinner(data.cards[0], data.cards[1]);
     });
 });
+
+function determineCardWinner(card1, card2) {
+  const cardOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+
+  const cardValue1 = cardOptions.indexOf(card1.value);
+  const cardValue2 = cardOptions.indexOf(card2.value);
+
+  if (cardValue1 > cardValue2) {
+    console.log("Computer Wins!");
+  } else if (cardValue1 < cardValue2) {
+    console.log("You Wins!");
+  } else {
+    console.log("It's a tie!!");
+  }
+}
